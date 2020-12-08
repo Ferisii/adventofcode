4835861c497d021503fa1d88b3c42c5fae3d1f3c
Ferisii <rwalther90@gmail.com>
2020-12-08 22:01:43 +0100

    added ori_operator and fixed +0 jmp (adds 1)

--- 2020/Day 8/day8_improved.py
+++ 2020/Day 8/day8_improved.py
@@ -7,7 +7,7 @@ def TheReturner(list_of_lines: list, index: int=0, acc: int=0):
     elif list_of_lines[index][0] == "nop":
         index += 1
     elif list_of_lines[index][0] == "jmp":
-        index += list_of_lines[index][1]
+        index += list_of_lines[index][1] if list_of_lines[index][1] != 0 else 1
     
     return index, acc
 
@@ -21,6 +21,7 @@ def ComputeList(list_of_lines: list, pathfinder: tuple=None) -> int:
             index, acc = TheReturner(list_of_lines, index, acc)
     else:
         for tmp_operator in pathfinder:
+            ori_operator = pathfinder[0] if tmp_operator == pathfinder[1] else pathfinder[1]
             for tmp_index in list_range:
                 if list_of_lines[tmp_index][0] in (tmp_operator, "acc"):
                     continue
@@ -29,8 +30,7 @@ def ComputeList(list_of_lines: list, pathfinder: tuple=None) -> int:
                     index_set.add(index)
                     index, acc = TheReturner(list_of_lines, index, acc)
                     if index in index_set: # Repeat found? reset
-                        list_of_lines[tmp_index][0] = pathfinder[0] if \
-                        list_of_lines[tmp_index][0] == pathfinder[1] else pathfinder[1]
+                        list_of_lines[tmp_index][0] = ori_operator
                         index, acc = 0, 0
                         index_set = set()
                         break
